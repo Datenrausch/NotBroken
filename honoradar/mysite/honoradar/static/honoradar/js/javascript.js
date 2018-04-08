22ede52be3ee7fd05503db78763d97646b555bb3
merekat <michel@penke.me>
2018-04-08 16:41:29 +0200

    Result Box Atmosphäre Style & Fixes

--- honoradar/mysite/honoradar/static/honoradar/js/javascript.js
+++ honoradar/mysite/honoradar/static/honoradar/js/javascript.js
@@ -771,21 +771,3 @@ $('.smooth').on('click', function(e){
 
 
 
-$(function() {
-  showMessage("Daten fehlen: Bitte fülle dieses Feld aus!");
-});
-
-function showMessage(s) {
-  var m = $("#alert");
-  m.html(s);
-  
-  m.addClass("show");
-  setTimeout(function() {
-    m.removeClass("show");
-    m.addClass("hide");
-    setTimeout(function() {
-      m.addClass("hide");
-    }, 5000);
-  }, 7000);
-}
-
