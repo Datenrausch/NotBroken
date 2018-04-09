2aa596ff2fc6e63ed63a1f693f7a44fd5de82245
HalukaMB <mail@maier-borst.de>
2018-04-10 01:11:24 +0200

    preventing default

--- honoradar/mysite/honoradar/static/honoradar/js/javascript.js
+++ honoradar/mysite/honoradar/static/honoradar/js/javascript.js
@@ -791,9 +791,10 @@ function smoothfunction(){
 }
 
 $('.smooth').on('click', function(e){
+  e.preventDefault();
+
   var href = $(this).attr('href');
   $('html, body').animate({
     scrollTop:$(href).offset().top
   },'slow');
-  e.preventDefault();
 })
